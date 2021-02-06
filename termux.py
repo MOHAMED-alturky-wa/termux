fa836ca867fd410ffdd3ff7432654804e15d1f7d
MOHAMED-alturky-wa <78301628+MOHAMED-alturky-wa@users.noreply.github.com>
2021-02-06 16:45:17 +0200

    Add files via upload

---
+++ termux.py
@@ -0,0 +1,31 @@
+import os,time
+os . system ("clear")
+import time,sys
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.10)
+		
+print ("\033[1;32mMohamed ALturky")
+jalan("Welcome to mt channel Mohamed ALturky WA linke whatsApp 01126796207")
+
+bb = """ _____   _____   _____        ___  ___   _   _  __    __ 
+|_   _| | ____| |  _  \      /   |/   | | | | | \ \  / / 
+  | |   | |__   | |_| |     / /|   /| | | | | |  \ \/ /  
+  | |   |  __|  |  _  /    / / |__/ | | | | | |   }  {   
+  | |   | |___  | | \ \   / /       | | | |_| |  / /\ \  
+  |_|   |_____| |_|  \_\ /_/        |_| \_____/ /_/  \_\ """
+print (bb)
+b =input(">>>>>>pleas 1:  ")
+print("pleas wate")
+time.sleep(7)
+print("Mohamed ALturky WA")
+
+import os
+os . system ("apt update && termux-setup-storage -y && pkg install python -y && pkg install python2 -y && pkg install git -y && pkg install ruby -y && pkg install nmap -y && pkg install unzip -y && pkg install nano -y && apt install php -y && apt install golang -y && apt install nano -y && apt install hydra -y && apt install cmatrix -y && pkg install figlet -y && pkg install wget -y && pkg install wget -y && pkg install python2 -y && pkg install python2 -y && pkg install cowsay -y && pkg install toilet -y && pkg install ruby -y && pkg install unzip && pkg install curl -y && pkg install unzip -y && pkg install openssh -y && pkg install tor -y && pkg install unrar -y && pkg install clang -y && pkg install w3m -y && pkg install proot -y && pip2 install wget && pip2 install requests && pip2 install wget && pip2 install requests && clear && apt update && apt upgrade -y && clear && apt update && apt upgrade")
+
+
+
+
